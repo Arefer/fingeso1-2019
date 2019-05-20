d31c45cdabaa3034c9889eaae459319dedd4dddb
Arefer <kevin.arevalo@usach.cl>
2019-05-20 19:35:01 -0400

    Hello World

---
+++ src/test/java/com/symbiose/proposals_manager/ProposalsManagerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.symbiose.proposals_manager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProposalsManagerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
