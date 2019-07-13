7b4cb9c25a48784ff95f7521f17cebbf8139e912
Arefer <kevin.arevalo@usach.cl>
2019-07-13 11:58:16 -0400

    Eliminada la capa de servicios

--- src/main/java/com/symbiose/proposals_manager/Controllers/ProposalController.java
+++ src/main/java/com/symbiose/proposals_manager/Controllers/ProposalController.java
@@ -1,7 +1,7 @@
 package com.symbiose.proposals_manager.Controllers;
 
+import com.symbiose.proposals_manager.DAO.ProposalRepository;
 import com.symbiose.proposals_manager.Models.Proposal;
-import com.symbiose.proposals_manager.Services.ProposalService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,16 +12,17 @@ import java.util.List;
 @RestController
 public class ProposalController {
     @Autowired
-    private ProposalService proposalService;
+    private ProposalRepository proposalRepository;
 
     @RequestMapping("/proposal/create")
     public String createProposal(@RequestParam String name, @RequestParam int client_id, @RequestParam int budget){
-        Proposal p = proposalService.create(name, client_id, budget);
+        //Proposal p = proposalService.create(name, client_id, budget);
+        Proposal p = proposalRepository.save(new Proposal(name, client_id, budget));
         return p.toString();
     }
 
     @RequestMapping("proposal/getall")
     public List<Proposal> getAllProposals(){
-        return proposalService.getAll();
+        return proposalRepository.findAll();
     }
 }
